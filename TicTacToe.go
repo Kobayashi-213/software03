6b3f0e906e36a80ef7868aaec5c5a3fc05b94eac
Kobayashi-213 <b170170@icloud.com>
2021-06-30 02:59:08 +0000

    finish

--- TicTacToe.go
+++ TicTacToe.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 type Board struct {
@@ -9,10 +10,12 @@ type Board struct {
 }
 
 func (b *Board) put(x, y int, u string) {
-	if u == "o" {
+
+	if b.get(x, y) != "." {
+		fmt.Println("Cannot put here!")
+	} else if u == "o" {
 		b.tokens[x+3*y] = 1
-	}
-	if u == "x" {
+	} else if u == "x" {
 		b.tokens[x+3*y] = 2
 	}
 }
@@ -32,9 +35,9 @@ func (b *Board) input(u string) {
 	//Player1 がxとする
 	var x, y int
 	if u == "x" {
-		fmt.Printf("Player 1: Input (x,y):")
-	} else if u == "o" {
 		fmt.Printf("Player 2: Input (x,y):")
+	} else if u == "o" {
+		fmt.Printf("Player 1: Input (x,y):")
 	}
 	//test用
 	x = 1
@@ -90,5 +93,39 @@ func (b *Board) hantei() int {
 }
 
 func main() {
+	b := &Board{
+		tokens: []int{0, 0, 0, 0, 0, 0, 0, 0, 0},
+	}
+	result := ""
 
+	for i := 0; i < 9; i++ {
+		if i%2 == 0 {
+			b.input("o")
+			for i := 0; i < 3; i++ {
+				result = ""
+				for j := 0; j < 3; j++ {
+					result += b.get(i, j)
+				}
+				fmt.Println(result)
+			}
+			if b.hantei() == 1 {
+				fmt.Println("Player 1 won")
+				os.Exit(3)
+			}
+		} else if i%2 != 0 {
+			b.input("x")
+			for i := 0; i < 3; i++ {
+				result = ""
+				for j := 0; j < 3; j++ {
+					result += b.get(i, j)
+				}
+				fmt.Println(result)
+			}
+			if b.hantei() == 2 {
+				fmt.Println("Player 2 won")
+				os.Exit(3)
+			}
+		}
+	}
+	fmt.Println()
 }
